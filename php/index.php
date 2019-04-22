1087e8a3f1bf1ee5e31e5ac6c9ca79c963c65a3c
luisdimas9 <37938811+luisdimas9@users.noreply.github.com>
2019-04-22 09:34:24 -0400

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,487 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="lib/responsive.css">	
+    <link rel="stylesheet" href="lib/materialize/css/materialize.css">
+	
+  </head>
+  <body>
+    <div class="col s12" style="margin: 0px !important; padding: 0px !important;">
+		<div class="card azul-special offset-s6" style="margin: 0px !important; padding: 0px !important; border-radius: 0px !important; border-botton: 0px !important; display: flex; justify-content: center;
+  align-items: center;">
+			<div class="card-content white-text">
+				<div class="col s2" style="height: 100px; width: 100px;">
+					<img src="asofutbol-logo.png" alt="" class="responsive-img" style="width: 100% !important; height: 100% !important;"> <!-- notice the "circle" class -->
+				</div>
+			</div>						
+		</div>
+	</div>
+	<nav class="fixed azul-special" style="border-bottom: 3px solid #ff4500 !important;">
+		<div class="nav-wrapper">
+		  <a href="#" class="brand-logo">AsoFutbol Monagas</a>
+		  <ul id="nav-mobile" class="right hide-on-med-and-down">
+			<li><a href="sass.html">Torneos</a></li>
+			<li><a href="badges.html">Noticias</a></li>
+			<li><a href="badges.html">Equipos</a></li>
+			<li><a href="collapsible.html">Contactanos</a></li>
+		  </ul>
+		</div>
+	</nav>     
+	<div class="slider card">
+		<ul class="slides"> 
+				<li>
+					<img class="responsive-img" src="image/aso.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3></h3>
+						<h5 class="light grey-text text-lighten-3"></h5>
+					</div>
+				</li>
+				<li>
+					<img class="responsive-img" src="image/playa.jpg"> <!-- random image -->
+					<div class="caption left-align">
+						<h3>Hola</h3>
+						<h5 class="light grey-text text-lighten-3">holaaaa</h5>
+					</div>
+				</li>
+				<li>
+					<img class="responsive-img" src="image/fem.jpg"> <!-- random image -->
+					<div class="caption right-align">
+						<h3>Hola</h3>
+						<h5 class="light grey-text text-lighten-3">holaaaa</h5>
+					</div>
+				</li>
+		</ul>
+	</div>
+<!--         publicidad             -->	
+	<div class="row">
+		<div class="col s12">
+			<div class="row">
+				<div class="col s6 offset-s3">
+					<div class="card azul-special" style="height: 180px !important; border-bottom: 3px solid #ff4500 !important;">
+						<img class="responsive-img" src="image/publi/maltin.jpg" style="width: 100% !important; height: 100% !important;">								
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--         fin publicidad            -->	
+        <div class="row">
+<!--         barra lateral izquierda             -->	
+		  <div class="col s3">
+			<div class="row">
+				<div class="col s12">
+					<div class="card z-depth-5 azul-special">
+						<div class="card-content white-text" style="border-bottom: 3px solid #ff4500 !important;">
+						  <span class="card-title">Posisiciones Serie Oro</span>
+						  <span class="card-title">Sub-16 Femenino</span>
+						  <table class="responsive-table">
+							<thead>
+							  <tr>
+								  <th>Name</th>
+								  <th>Item Name</th>
+								  <th>Item Price</th>
+							  </tr>
+							</thead>
+
+							<tbody>
+							  <tr>
+								<td>Alvin</td>
+								<td>Eclair</td>
+								<td>$0.87</td>
+							  </tr>
+							  <tr>
+								<td>Alan</td>
+								<td>Jellybean</td>
+								<td>$3.76</td>
+							  </tr>
+							  <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							  
+							</tbody>
+						  </table>
+						</div>
+						
+					</div>
+				</div>
+			</div>			
+		  </div>
+		  
+<!--       final de la barra lateral izquierda            -->
+<!--       centro inside            -->			  
+		  <div class="col s6">
+			<div class="row">
+				<div class="col s12" style="border-bottom: 3px solid #ff4500 !important;">				  
+					  <h4 class="white-text">Noticias Destacadas</h4>				
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m6">
+					<div class="card horizontal azul-special z-depth-5" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-image">
+							<img src="https://lorempixel.com/100/190/nature/6">
+						</div>
+						<div class="card-stacked">
+							<div class="card-content white-text">
+								<p>I am a very simple card. I am good at containing small bits of information.</p>
+							</div>
+							<div class="card-action">
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		 </div>
+<!--       fin centro inside            -->	
+<!--       barra lateral derecha           -->			
+		  <div class="col s3">
+			<div class="row">
+				<div class="col s12">
+					<div class="card z-depth-5 azul-special" style="border-bottom: 3px solid #ff4500 !important;">
+						<div class="card-content white-text">
+						  <span class="card-title">Card Title</span>
+						  <table class="responsive-table">
+							<thead>
+							  <tr>
+								  <th>Name</th>
+								  <th>Item Name</th>
+								  <th>Item Price</th>
+							  </tr>
+							</thead>
+
+							<tbody>
+							  <tr>
+								<td>Alvin</td>
+								<td>Eclair</td>
+								<td>$0.87</td>
+							  </tr>
+							  <tr>
+								<td>Alan</td>
+								<td>Jellybean</td>
+								<td>$3.76</td>
+							  </tr>
+							  <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							   <tr>
+								<td>Jonathan</td>
+								<td>Lollipop</td>
+								<td>$7.00</td>
+							  </tr>
+							  
+							</tbody>
+						  </table>
+						</div>						
+					</div>
+				</div>
+			</div>			
+		  </div>
+<!--      fin barra lateral derecha           -->	
+		</div>
+	
+<!--      publicidad           -->		
+		<div class="row">
+			<div class="col s12">
+				<div class="row">
+					<div class="col s6 offset-s3">
+						<div class="card azul-special" style="height: 180px !important; border-bottom: 3px solid #ff4500 !important;">
+							<img class="responsive-img" src="image/publi/brahma.jpg" style="width: 100% !important; height: 100% !important;">								
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--      fin publicidad           -->
+<!--      panel informativo           -->
+		<div class="row">
+			<div class="col s12" style="border-bottom: 3px solid #ff4500 !important;">				  
+				<h4 class="white-text">Entrevistas De La Semana</h4>				
+			</div>
+		</div>				
+		<div class="row">
+			<div class="col s5">
+				<div class="row">
+					<div class="col s12">
+						<div class="card azul-special z-depth-5" style="height:412px; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-image waves-effect waves-block waves-light" style="height:300px;">
+								<img class="activator" src="image/port.jpg">
+							</div>
+							<div class="card-content">
+								<span class="card-title activator grey-text">Vamos Al Mundial<i class="material-icons right">more_vert</i></span>
+								<p><a href="#">Ver</a></p>
+							</div>
+							<div class="card-reveal" style="background:rgba(0,0,51, 0.7);">
+								<span class="card-title grey-text">Vamos Al Mundial<i class="material-icons right">close</i></span>
+								<p class="white-text">Here is some more information about this product that is only revealed once clicked on.</p>
+							</div>							
+						</div>
+					</div>
+				</div>			
+			</div>
+			<div class="col s7" style="height:412px;">
+				<div class="row">
+					<div class="col s12">
+						<div class="card azul-special z-depth-5" style="height:412px; padding: 0px; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-content white-text" style=" padding: 0px !important;">
+								<iframe style="border-style: none;width: 100%; height: 412px; frameborder="0" src="https://www.youtube.com/embed/85MppyLJHz0?autohide=0" allowfullscreen></iframe>
+							</div>							
+						</div>
+					</div>
+				</div>			
+			</div>
+		</div>
+<!--     fin panel informativo           -->
+<!--     round publicitario           -->		
+		<div class="row">
+			<div class="col s12">
+				<div class="row">
+					<div class="col s6 m3">
+						<div class="card azul-special z-depth-5" style="height: 200px !important; border-bottom: 3px solid #ff4500 !important;">							
+							<img class="responsive-img" src="image/publi/JAC.jpg" style="width: 100% !important; height: 100% !important;">														
+						</div>
+					</div>
+					<div class="col s6 m3">
+						<div class="card azul-special z-depth-5" style="height: 200px !important; border-bottom: 3px solid #ff4500 !important;">
+							<img class="responsive-img" src="image/publi/fanta.jpeg" style="width: 100% !important; height: 100% !important;">								
+						</div>
+					</div>
+					<div class="col s6 m3">
+						<div class="card azul-special z-depth-5" style="height: 200px !important; border-bottom: 3px solid #ff4500 !important;">
+							<img class="responsive-img" src="image/publi/ego.jpg" style="width: 100% !important; height: 100% !important;">							
+						</div>
+					</div>
+					<div class="col s6 m3">
+						<div class="card azul-special z-depth-5" style="height: 200px !important; border-bottom: 3px solid #ff4500 !important;">
+							<img class="responsive-img" src="image/publi/swda.jpg" style="width: 100% !important; height: 100% !important;">								
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--  videos destacados -->
+		<div class="row">
+			<div class="col s8 offset-s2" style="border-bottom: 3px solid #ff4500 !important;">				  
+				<h4 class="white-text">Videos De La Semana</h4>				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s8 offset-s2">				  
+				<div class="row">
+					<div class="col s12 m6">
+						<div class="card azul-special" style="height: 250px !important; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-content white-text" style=" padding: 0px !important;">
+								<iframe style="border-style: none;width: 100%; height: 250px; frameborder="0" src="https://www.youtube.com/embed/fJFk-4w9m4M?autohide=0" allowfullscreen></iframe>
+							</div>							
+						</div>
+					</div>
+					<div class="col s12 m6">
+						<div class="card azul-special" style="height: 250px !important; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-content white-text" style=" padding: 0px !important;">
+								<iframe style="border-style: none;width: 100%; height: 250px; frameborder="0" src="https://www.youtube.com/embed/lTjQH_SXmSY?autohide=0" allowfullscreen></iframe>
+							</div>						
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col s12 m6">
+						<div class="card azul-special" style="height: 250px !important; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-content white-text" style=" padding: 0px !important;">
+								<iframe style="border-style: none;width: 100%; height: 250px; frameborder="0" src="https://www.youtube.com/embed/fJFk-4w9m4M?autohide=0" allowfullscreen></iframe>
+							</div>							
+						</div>
+					</div>
+					<div class="col s12 m6">
+						<div class="card azul-special" style="height: 250px !important; border-bottom: 3px solid #ff4500 !important;">
+							<div class="card-content white-text" style=" padding: 0px !important;">
+								<iframe style="border-style: none;width: 100%; height: 250px; frameborder="0" src="https://www.youtube.com/embed/lTjQH_SXmSY?autohide=0" allowfullscreen></iframe>
+							</div>						
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--  fin de videos destacados -->
+	<footer class="page-footer azul-special">
+         <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
+ 
+	<script src="lib/materialize/js/jquery.js"></script>
+    <script src="lib/materialize/js/materialize.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		
+		$('.slider').slider({
+            indicators: false,
+            height: 500,
+            transition: 1000,
+            interval: 7000            
+        });
+		
+	});
+	</script>
+  </body>
+</html>
